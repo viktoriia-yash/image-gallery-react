f82b0b1bf7accd1849f15dfb3bd329f3c456a3a9
viktoriia-yash <yashkina.vic@gmail.com>
2023-07-03 22:50:29 +0300

    base code for header + styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="bg-black flex items-center py-10">
+      <div className="max-w-md mx-auto w-full">
+        <h1 className="text-white text-center text-2xl font-bold mb-5">
+          Image Gallery
+        </h1>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
